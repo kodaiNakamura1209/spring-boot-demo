b4af00cfd3604723475d6bf2c68c851776238239
nakamurakodai <natural.green1209@ezweb.ne.jp>
2024-03-05 18:34:42 +0900

    ログイン認証を追加

--- src/main/java/com/example/demo/Config/SecurityConfig.java
+++ src/main/java/com/example/demo/Config/SecurityConfig.java
@@ -12,24 +12,41 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                // アクセス制限
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                        .requestMatchers("/").permitAll()
+        http.formLogin(login -> login
+                // 指定したURLがリクエストされるとログイン認証を行う。
+                .loginProcessingUrl("/login")
 
-                        // 他のリンクは全て認証が必要
-                        .anyRequest().authenticated()
-                )
-                .formLogin((form) -> form
-                        // ログインページへのパスを指定→コントローラーにもGET、/loginでの処理を記載する必要がある
-                        .loginPage("/login")
-                        // ログイン成功時に表示される画面へのパス
-                        .defaultSuccessUrl("/menu")
-                        .permitAll()
-                )
-                .logout((logout) -> logout.permitAll());
+                // ログイン時のURLの指定
+                .loginPage("/login")
 
+                // 認証成功後にリダイレクトする場所の指定
+                .defaultSuccessUrl("/menu")
+
+                // ログインに失敗した時のURL
+                .failureUrl("/loginError")
+
+                // login.htmlのユーザーIDのname
+                .usernameParameter("user_id")
+
+                // login.htmlのパスワードのname
+                .passwordParameter("password")
+
+                //アクセス権限の有無（permitAllは全てのユーザーがアクセス可能)
+                .permitAll()
+        ).logout(logout -> logout
+                        .logoutSuccessUrl("/")
+
+                //アクセス制限
+        ).authorizeHttpRequests(ahr -> ahr
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                .requestMatchers("/").permitAll()
+
+                //"/admin"はADMIN権限のあるものだけがアクセスできる
+                .requestMatchers("/admin").hasRole("ADMIN")
+
+                //他のリンクは全て認証が必要である。
+                .anyRequest().authenticated()
+        );
         return http.build();
     }
 }
